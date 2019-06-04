359631b92f00ad8205f518d153454865c71f7cc5
Sergey Dreval <sergey.dreval@auto1.com>
2019-06-04 19:48:17 +0300

    fixing kafka problems

--- data-receiver-service/src/test/java/com/blogspot/mvnblogbuild/telemetry/storage/DataReaderControllerIntegrationTest.java
+++ data-receiver-service/src/test/java/com/blogspot/mvnblogbuild/telemetry/storage/DataReaderControllerIntegrationTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -26,6 +27,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = DataReceiverApplication.class)
 @AutoConfigureMockMvc
+@EmbeddedKafka(partitions = 1, controlledShutdown = false,
+        brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 public class DataReaderControllerIntegrationTest {
 
     @Autowired
