080ebedd643e3f4de9033e1a983b2758a4cc8f47
Sergey Dreval <sergey.dreval@auto1.com>
2020-02-18 13:07:35 +0200

    add AWS test project

---
+++ aws-test-service/src/main/java/com/blogspot/mvnblogbuild/telemetry/awstest/AWSTestApplication.java
@@ -0,0 +1,81 @@
+package com.blogspot.mvnblogbuild.telemetry.awstest;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.util.List;
+
+@SpringBootApplication
+public class AWSTestApplication implements CommandLineRunner {
+
+    private static Logger LOG = LoggerFactory.getLogger(AWSTestApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(AWSTestApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String bucketName = "testsergey";
+        String awsAccessKey = System.getenv("AWS_ACCESS_KEY");
+        String awsSecretKey = System.getenv("AWS_SECRET_KEY");
+        LOG.info("Login using AWS key {}...", awsAccessKey != null ? awsAccessKey.substring(0, 5): null);
+
+        AWSCredentials credentials = new BasicAWSCredentials(
+                awsAccessKey,
+                awsSecretKey
+        );
+        AmazonS3 s3client = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(Regions.EU_CENTRAL_1)
+                .build();
+
+
+        List<Bucket> buckets = s3client.listBuckets();
+        buckets.forEach(bucket -> {
+            LOG.info("BUCKET: {}", bucket.getName());
+        });
+
+        createBucket(s3client, bucketName);
+
+        ObjectListing objectListing = s3client.listObjects(bucketName);
+        for(S3ObjectSummary os : objectListing.getObjectSummaries()) {
+            LOG.info(os.getKey());
+        }
+
+    }
+
+    private void createBucket(AmazonS3 s3client, String bucketName) {
+
+        if(s3client.doesBucketExist(bucketName)) {
+            LOG.info("Bucket name is not available."
+                    + " Try again with a different Bucket name.");
+            return;
+        }
+
+        s3client.createBucket(bucketName);
+    }
+
+    private void createObject(AmazonS3 s3client, String bucketName) {
+        s3client.putObject(
+                bucketName,
+                "Document/hello.txt",
+                new File("/Users/user/Document/hello.txt")
+        );
+    }
+
+}
---
+++ aws-test-service/src/test/java/com/blogspot/mvnblogbuild/telemetry/awstest/AWSTestApplicationTest.java
@@ -0,0 +1,16 @@
+package com.blogspot.mvnblogbuild.telemetry.awstest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AWSTestApplication.class)
+public class AWSTestApplicationTest {
+
+    @Test
+    public void testReader() throws Exception {
+    }
+
+}
